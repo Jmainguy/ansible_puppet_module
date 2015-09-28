8333431e0db0c92165544a3fb2c8ac91fdd01c05
Jonathan Mainguy <jmainguy@redhat.com>
2015-09-28 15:43:32 -0400

    have stdout and stderr and rc and stuff

--- puppet.py
+++ puppet.py
@@ -159,7 +159,7 @@ def main():
 
     # 2 means stuff changed, 4 means errors, 6 means stuff changed and errors
     if rc != 2:
-        module.fail_json(msg='Puppet encountered errors. %s %s' % (stdout, stderr) )
+        module.fail_json(msg='Puppet encountered errors. %s %s' % (stdout, stderr), rc=rc, stdout=stdout, stderr=stderr )
     else:
         rc = 0
 
